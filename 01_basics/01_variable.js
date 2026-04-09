cd82be801866d94517796ff11307977d79611d45
djangid-collab <hey.djangid@gmail.com>
2026-04-09 12:41:52 +0000

    container config done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,18 @@
+const accountId=89526895
+let accountEmail = "Hey.djangid@gmail.com"
+var accountPassword = "12345678"
+ accountCity = "Jaipur"
+ let accountState;
+
+// accountId = 2 // not allowed
+
+accountEmail="dipesh@dj.com"
+accountpassword="123456789"
+accountCity="Ajmer"
+console.log(accountId)
+/*
+prefer not to use var
+because of issue in block scope and function scope
+*/
+console.table([accountEmail,accountPassword,accountCity, accountState]);   
+
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use Stict";  // treat all JS code as newer version
+
+// alert (3+3)// we are using node.js , not browser
+console.log(3
+    +3) // code readability should be high
+
+    console.log("Dipesh jangid")
+
+    let name = "Dipesh jangid"
+    let age=    21
+    let isLoggedin = false
+
+    // number => 2 to power 53
+    //bigint
+    //string
+    //boolen  => true or false 
+    //null => empty value
+    //undefined => value is not assigned
+    //symbol => unique value
+
+    
+
+
+    // object
+     
+    console.log(typeof age);
